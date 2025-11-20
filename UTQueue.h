99a87319a790cd22240c95f09cdd870536af40e3
ViditaBhotika <bvidita@gmail.com>
2025-11-20 12:16:15 -0600

    Add files via upload

---
+++ UTQueue.h
@@ -0,0 +1,32 @@
+#ifndef UTQUEUE_H
+#define UTQUEUE_H 1
+
+#include <stdint.h>
+
+
+// Struct and function signatures were provided prior to starting project
+
+typedef struct UTQueue {
+  uint32_t size;
+  int *data;
+} UTQueue;
+
+void utqueueprint(UTQueue *src);
+
+UTQueue *utqueuedup(const int *src, int n);
+
+void utqueuefree(UTQueue *self);
+
+UTQueue *utqueuepush(UTQueue *src, int value);
+
+int utqueuepop(UTQueue *src);
+
+UTQueue *utqueuerev(UTQueue *src);
+
+UTQueue *utqueuecombine(UTQueue *dst, UTQueue *src);
+
+void utqueueswap(UTQueue *q1, UTQueue *q2);
+
+UTQueue *utqueuecpy(UTQueue *dst, const int *src, int n);
+
+#endif // !UTQUEUE_H
